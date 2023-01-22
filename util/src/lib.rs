c9fbe5f320262ccef06f1579de5d6847cc06c46b
Levente Gyozo Lenart <lgylenart@lgylen.art>
2023-01-22 11:54:12 +0100

    util: Add smart_enum macro

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,1 +1,25 @@
 pub mod job;
+
+#[macro_export]
+macro_rules! smart_enum {
+    ($v:vis, $i:ident, $($j:ident),*) => {
+        #[derive(Copy, Clone)]
+        $v enum $i {
+            $($j),*
+        }
+
+        impl $i {
+            pub fn to_string(&self) -> &str {
+                (*self).into()
+            }
+        }
+
+        impl From<$i> for &str {
+            fn from(value: $i) -> Self {
+                match value {
+                    $($i::$j => stringify!($j)),*
+                }
+            }
+        }
+    };
+}
