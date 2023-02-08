28f9f16b59c0a1ccb030eb9f2aa81f11d8b9fd48
Levente Gyozo Lenart <lgylenart@lgylen.art>
2023-02-08 21:40:12 +0100

    game: Use OpenGlRenderer

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,21 +1,27 @@
 use std::io::BufWriter;
 use std::io::{self};
+use std::num::NonZeroU32;
 use std::time::Instant;
 
+use engine::renderer::opengl_renderer::OpenGlRenderer;
+use engine::renderer::Renderer;
 use engine::scene::Scene;
 use engine::Engine;
 use engine::EngineThreadCategory;
 use helper::translate_winit_keyboard_event;
+use raw_window_handle::HasRawDisplayHandle;
+use raw_window_handle::HasRawWindowHandle;
 use util::job::Scheduler;
 use util::logger::create_logger;
 use util::logger::LogSeverity::Debug;
 use util::logger::LogSeverity::Info;
 use util::thread_pool_descriptor;
+use winit::dpi::PhysicalSize;
 use winit::event::Event;
 use winit::event::KeyboardInput;
 use winit::event::WindowEvent;
 use winit::event_loop::EventLoop;
-use winit::window::Window;
+use winit::window::WindowBuilder;
 
 mod helper;
 
@@ -38,12 +44,34 @@ fn main() {
             .log(Debug, "Hello from logic component!");
     });
 
-    let engine = Engine::new(scheduler, logger_client, scene);
+    let engine = Engine::new(scheduler, logger_client, scene.clone());
 
     let event_loop = EventLoop::new();
-    let _window = Window::new(&event_loop).unwrap();
-
     let mut previous_update = Instant::now();
+    let inner_size = PhysicalSize::new(1024, 768);
+    let window = WindowBuilder::new()
+        .with_inner_size(inner_size)
+        .build(&event_loop)
+        .unwrap();
+
+    #[cfg(target_os = "linux")]
+    let renderer = OpenGlRenderer::new(
+        window.raw_display_handle(),
+        window.raw_window_handle(),
+        NonZeroU32::new(inner_size.width).unwrap(),
+        NonZeroU32::new(inner_size.height).unwrap(),
+        Box::new(winit::platform::x11::register_xlib_error_hook),
+    );
+
+    #[cfg(target_os = "windows")]
+    let renderer = OpenGlRenderer::new(
+        window.raw_display_handle(),
+        window.raw_window_handle(),
+        NonZeroU32::new(inner_size.width).unwrap(),
+        NonZeroU32::new(inner_size.height).unwrap(),
+        (),
+    );
+
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
 
@@ -54,6 +82,7 @@ fn main() {
                 previous_update = now;
 
                 engine.update(delta_time);
+                renderer.render(&scene);
             }
             Event::WindowEvent {
                 event:
@@ -73,6 +102,12 @@ fn main() {
                     virtual_key_code,
                     state,
                 );
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                renderer.resize(size.width as usize, size.height as usize);
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
